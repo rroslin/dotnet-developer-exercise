3bd3500c7445e49e6c53c4a17f67b539ead1845e
rroslin <raymark.roslin@gmail.com>
2025-04-28 07:41:32 +0800

    chore: add controller to services

--- Api/Program.cs
+++ Api/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 builder.Services.AddHealthChecks();
 
 // Add and configure database context
